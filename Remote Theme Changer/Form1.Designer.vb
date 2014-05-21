6154dcaebb6d16409e83c146fabfaf25b063701f
scarfacecapwn <mattinator@comcast.net>
2014-05-21 15:31:07 -0500

    Datagridview and strAdditionalArguments added

--- Remote Theme Changer/Form1.Designer.vb
+++ Remote Theme Changer/Form1.Designer.vb
@@ -22,17 +22,50 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.gridPCList = New System.Windows.Forms.DataGridView()
+        Me.colPCList = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.gridPCList, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'gridPCList
+        '
+        Me.gridPCList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.gridPCList.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.colPCList})
+        Me.gridPCList.Location = New System.Drawing.Point(12, 12)
+        Me.gridPCList.Name = "gridPCList"
+        Me.gridPCList.Size = New System.Drawing.Size(387, 238)
+        Me.gridPCList.TabIndex = 2
+        '
+        'colPCList
+        '
+        Me.colPCList.HeaderText = "PC Name"
+        Me.colPCList.Name = "colPCList"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(457, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ClientSize = New System.Drawing.Size(628, 262)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.gridPCList)
         Me.Name = "Form1"
         Me.Text = "Remote Theme Changer"
+        CType(Me.gridPCList, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents gridPCList As System.Windows.Forms.DataGridView
+    Friend WithEvents colPCList As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
--- Remote Theme Changer/Form1.vb
+++ Remote Theme Changer/Form1.vb
@@ -1,3 +1,26 @@
 ﻿Public Class Form1
+    Dim strAdditionalArguments As String
+    Dim arrPCList As New ArrayList
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        colPCList.Width = 344
+    End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For Each row As DataGridViewRow In gridPCList.Rows
+            arrPCList.Add(row.Cells(0).Value)
+        Next
+
+        For i As Integer = 0 To arrPCList.Count - 1
+            strAdditionalArguments += arrPCList(i) & " "
+        Next
+
+        '   Dear Shell,
+        '       Please run that script that changes themes remotely with the provided arguments. You will find enclosed
+        '   a strAdditionalArguments that can be quite helpful. Thanks, bro.
+
+        '   Regards,
+
+        '   Matt Gleason
+
+    End Sub
 End Class
